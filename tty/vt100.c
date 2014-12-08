4a1c39b6c7514014597712670adfce9e0e8fcd8e
Your Name <you@example.com>
2014-12-08 19:21:29 +0100

    ..

--- tty/vt100.c
+++ tty/vt100.c
@@ -1,21 +1,24 @@
 /**
-	This file is part of FORTMAX.
+	This file is part of martink project.
 
-	FORTMAX is free software: you can redistribute it and/or modify
+	martink firmware project is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	FORTMAX is distributed in the hope that it will be useful,
+	martink firmware is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with FORTMAX.  If not, see <http://www.gnu.org/licenses/>.
+	along with martink firmware.  If not, see <http://www.gnu.org/licenses/>.
 
-	Copyright: Martin K. Schröder (info@fortmax.se) 2014
+	Author: Martin K. Schröder
+	Email: info@fortmax.se
+	Github: https://github.com/mkschreder
 */
+
 #include <stdint.h>
 #include <ctype.h>
 #include <math.h>
--- tty/vt100.h
+++ tty/vt100.h
@@ -1,20 +1,22 @@
 /**
-	This file is part of FORTMAX kernel.
+	This file is part of martink project.
 
-	FORTMAX kernel is free software: you can redistribute it and/or modify
+	martink firmware project is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	FORTMAX kernel is distributed in the hope that it will be useful,
+	martink firmware is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with FORTMAX kernel.  If not, see <http://www.gnu.org/licenses/>.
+	along with martink firmware.  If not, see <http://www.gnu.org/licenses/>.
 
-	Copyright: Martin K. Schröder (info@fortmax.se) 2014
+	Author: Martin K. Schröder
+	Email: info@fortmax.se
+	Github: https://github.com/mkschreder
 */
 
 #pragma once

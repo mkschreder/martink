d22a5828d28cb49b937bdf0fbce54a326c1fcdf5
Martin Schröder <info@nordiskamarknadssystem.se>
2015-02-08 05:05:27 +0100

    Added much needed stm32 peripheral lib files for multiple st devices

---
+++ arch/arm/stm32/usb/hw_config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <arch/soc.h>
+#include <inttypes.h>
+
+#define         ID1          (0x1FFFF7E8)
+#define         ID2          (0x1FFFF7EC)
+#define         ID3          (0x1FFFF7F0)
---
+++ arch/arm/stm32/usb/usb_conf.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define IMR_MSK (CNTR_CTRM  | CNTR_WKUPM | CNTR_SUSPM | CNTR_ERRM  | CNTR_SOFM \
+                 | CNTR_ESOFM | CNTR_RESETM )

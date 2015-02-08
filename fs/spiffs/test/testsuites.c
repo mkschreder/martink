96b4867cc1301973a227ad8c33739c16070545a8
Martin Schröder <info@nordiskamarknadssystem.se>
2015-02-08 05:16:00 +0100

    ..

---
+++ fs/spiffs/test/testsuites.c
@@ -0,0 +1,14 @@
+/*
+ * testsuites.c
+ *
+ *  Created on: Jun 19, 2013
+ *      Author: petera
+ */
+
+#include "testrunner.h"
+
+void add_suites() {
+  //ADD_SUITE(dev_tests);
+  ADD_SUITE(check_tests);
+  ADD_SUITE(hydrogen_tests)
+}

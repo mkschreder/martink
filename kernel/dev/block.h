9243a427bdd7fccca3f294bdc5d77c1931e435c7
Martin Schröder <info@nordiskamarknadssystem.se>
2015-03-10 17:58:09 +0100

    Made sure most of the projects compile. AVR still in progress

--- kernel/dev/block.h
+++ kernel/dev/block.h
@@ -3,8 +3,9 @@
 struct block_device; 
 typedef struct block_device **block_dev_t; 
 
-typedef uint16_t page_address_t; 
+typedef uint16_t blk_address_t; 
 
+#define 
 typedef enum {
 	BLKDEV_BUSY = (1 << 0)
 } blkdev_status_t; 
@@ -18,8 +19,9 @@ struct block_device_geometry {
 struct block_device {
 	uint8_t (*open)(block_dev_t self); 
 	int8_t 	(*close)(block_dev_t self); 
-	ssize_t (*writepage)(block_dev_t self, page_address_t page, const uint8_t *data, ssize_t data_size); 
-	ssize_t (*readpage)(block_dev_t self, page_address_t page, uint8_t *data, ssize_t data_size); 
+	ssize_t (*write)(block_dev_t self, const uint8_t *data, ssize_t data_size); 
+	ssize_t (*read)(block_dev_t self, blk_address_t address, uint8_t *data, ssize_t data_size); 
+	ssize_t (*seek)(block_dev_t self, ssize_t ofs, int whence);  
 	int8_t 	(*get_geometry)(block_dev_t self, struct block_device_geometry *geom); 
 	uint8_t (*get_status)(block_dev_t self, blkdev_status_t flags); 
 }; 
